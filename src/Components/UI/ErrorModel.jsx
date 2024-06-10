81f34acf0a16b3285b1eca1ac7d3552e291c70bc
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-06-10 18:45:05 +0300

    Update ErrorModel.jsx

--- src/Components/UI/ErrorModel.jsx
+++ src/Components/UI/ErrorModel.jsx
@@ -4,8 +4,8 @@ import Card from './Card'
 const ErrorModel = () => {
   return (
     <div className='fixed mx-auto flex justify-center top-1/4 left-0 w-screen z-50'>
-    <Card className="w-[36rem]">
-        <header >
+    <Card className="w-[36rem] p-0">
+        <header 'bg-red-700 p-4 text-white rounded-t-xl' >
             <h2>İsim Alanı Zorunludur</h2>
         </header>
         <section>
