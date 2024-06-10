2f6118734a3f2cf5c1b8812709f9b6ee1a280346
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-06-10 19:46:54 +0300

    Update ErrorModel.jsx

--- src/Components/UI/ErrorModel.jsx
+++ src/Components/UI/ErrorModel.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import Button from './Button'
-import Card from './Card'
-const ErrorModel = () => {
+import React from "react";
+import Button from "./Button";
+import Card from "./Card";
+const ErrorModel = (props) => {
+  const { onConfrim, error } = props;
+  const { title, message } = error;
   return (
-    <div className='Errormodal'>
-      <div className='backdrop-blur-sm bg-white/30 fixed w-screen h-screen top-0'></div>
-    <Card className="w-[36rem] p-0 z-20">
-        <header 'bg-red-700 p-4 text-white rounded-t-xl' >
-            <h2 className="text-center text-xl">İsim Alanı Zorunludur</h2>
+    <div className="errormodal">
+      <div
+        className="backdrop-blur-sm bg-white/30 fixed w-screen h-screen top-0"
+        onClick={onConfrim}
+      ></div>
+      <Card className="w-[36rem]  p-0 z-20">
+        <header className="bg-red-700 p-4 text-white rounded-t-xl ">
+          <h2 className="text-center text-xl">{title}</h2>
         </header>
-        <section>
-            <span>Lütfen Bir İsim Giriniz</span>
-        </section  className='p-4'>
-        <footer  className='p-4 justify-end'>
-            <Button className="w-32">Tamam</Button>
+        <section className="p-4 ">{message}</section>
+        <footer className="p-4 flex justify-end">
+          <Button onClick={onConfrim} className="w-32">
+            Tamam
+          </Button>
         </footer>
-    </Card>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorModel
+export default ErrorModel;
