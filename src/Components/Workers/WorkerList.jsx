b558f8e2fca3a3bdd5d9cf87b3e1e52447da0d26
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-06-10 18:24:29 +0300

    Update WorkerList.jsx

--- src/Components/Workers/WorkerList.jsx
+++ src/Components/Workers/WorkerList.jsx
@@ -2,22 +2,33 @@ import React from 'react'
 import Card from '../UI/Card'
 
 const WorkerList = (props) => {
+    const { workers, setWorkers } = props;
+  if (workers.length<1) {
+    return;
+  }
+    const deleteWorker = (id) => {
+    setWorkers(
+      workers.filter((item) => item.id !== id)
+    );
+  };
   return (
-    <Card className='mt-10'>
-        <ul>
-            <li className='flex justify-between p-2'>
-                <span className='font-bold'>İsim</span>
-                <span className='font-bold'>Maaş</span>
-            </li>
-            {props?.workers.map((workers)=>(
-                <li className='flex justify-between cursor-pointer hover:shadow-xl p-2 duration-100'>
-                <span>
-                    Emin Başbayan
-                </span>
-                <span>6000</span>
-            </li>
-            ))}
-        </ul>
+   <Card className="mt-10">
+      <ul>
+        <li className="flex justify-between p-2">
+          <span className="font-bold">İsim</span>
+          <span className="font-bold">Maaş</span>
+        </li>
+        {workers.map((worker) => (
+          <li
+            className="flex justify-between cursor-pointer hover:shadow-xl p-2 duration-100"
+            key={worker.id}
+            onClick={() => deleteWorker(worker.id)}
+          >
+            <span>{worker.name}</span>
+            <span>{worker.wage} $</span>
+          </li>
+        ))}
+      </ul>
     </Card>
   )
 }
