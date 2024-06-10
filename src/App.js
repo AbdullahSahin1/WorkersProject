71e7409f4ee8561ea53fd663b58ee4d80cf3fb38
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-06-10 16:58:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
     <h1 className='text-white mt-6 text-center text-3xl'>Maaş Otomasyonu</h1>
       <AddWorker></AddWorker>
+        <WorkerList workers = {[]}> </WorkerList>
     </div>
   );
 }
